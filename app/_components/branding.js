d181191284e7a89acb84bc64ebfc89d7c90c0592
Raquel Ferreira <44173946+raquel-f@users.noreply.github.com>
2023-06-30 16:22:04 +0100

    logo update

--- app/_components/branding.js
+++ app/_components/branding.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Branding() {
     return (
         <Link href={"/"} className="flex items-center">
-            <Image src={'/logo-high.png'} className="mr-3 w-auto h-9" alt="Simmer Eats Logo" height={40} width={150} priority={true} />
+            <Image src={'/logo-v2.png'} className="mr-3 w-auto h-9" alt="Simmer Eats Logo" height={80} width={400} priority={true} />
         </Link>
     );
 }
